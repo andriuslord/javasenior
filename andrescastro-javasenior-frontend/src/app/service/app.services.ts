81774926b8d754de29f20626066cc1ca6c4dec26
andriuslord <memoandrescm@gmail.com>
2020-06-11 01:19:59 -0400

    nueva actualizacion frontend

--- andrescastro-javasenior-frontend/src/app/service/app.services.ts
+++ andrescastro-javasenior-frontend/src/app/service/app.services.ts
@@ -7,30 +7,6 @@ export class AppService {
 
   constructor(private http: HttpClient) {
   }
-
-  consultar(rut): Promise<any> {
-    const promise = new Promise((resolve, reject) => {
-      const headers = new HttpHeaders({
-        'Content-Type': 'application/json'
-      });
-
-      let req;
-      req = {}
-      // const url = environment.bffUrl + '/api/wpay/pagar';
-      const url = 'http://localhost:9090/client/' + rut;
-      this.http.get(url, req).toPromise()
-        .then(
-          res => {
-            console.log(res);
-            resolve(res);
-          },
-          msg => {
-            reject(msg);
-          }
-        );
-    });
-    return promise;  }
-
   create(req): Promise<any> {
     const promise = new Promise((resolve, reject) => {
       const headers = new HttpHeaders({
@@ -52,48 +28,7 @@ export class AppService {
     return promise;
   }
 
-  editar(req): Promise<any> {
-    const promise = new Promise((resolve, reject) => {
-      const headers = new HttpHeaders({
-        'Content-Type': 'application/json'
-      });
 
-      // const url = environment.bffUrl + '/api/wpay/pagar';
-      const url = 'http://localhost:9090/client/editar';
-      this.http.put(url, req, {headers}).toPromise()
-        .then(
-          res => {
-            console.log(res);
-            resolve(res);
-          },
-          msg => {
-            reject(msg);
-          }
-        );
-    });
-    return promise;
-  }
 
-  agregar(req): Promise<any> {
-    const promise = new Promise((resolve, reject) => {
-      const headers = new HttpHeaders({
-        'Content-Type': 'application/json'
-      });
-
-      // const url = environment.bffUrl + '/api/wpay/pagar';
-      const url = 'http://localhost:9090/client/';
-      this.http.post(url, req, {headers}).toPromise()
-        .then(
-          res => {
-            console.log(res);
-            resolve(res);
-          },
-          msg => {
-            reject(msg);
-          }
-        );
-    });
-    return promise;
-  }
 
 }
